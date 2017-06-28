60b4b93f9b74e93f113563ca4b4277b9a20f35d3
varchala ganugapati <varchala@lincolner.com>
2017-06-28 23:15:43 +0530

    js change

--- js/jquery-1.1.js
+++ js/jquery-1.1.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $("#about").click(function() {
         $("#panelp").slideToggle("slow");
-
+        $("#panelv").hide();
+        $("#panelq").hide();
     });
 
 });
 $(document).ready(function() {
     $("#places").click(function() {
         $("#panelq").slideToggle("slow");
+        $("#panelv").hide();
+        $("#panelp").hide();
 
     });
 
@@ -16,6 +19,8 @@ $(document).ready(function() {
     $("#video").click(function() {
         $("#panelv").slideToggle("slow");
         $('#panelv').prop("controls autoplay", true);
+        $("#panelq").hide();
+        $("#panelp").hide();
 
     });
 
